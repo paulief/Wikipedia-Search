9efc0a49efb474086462c456f3e94bffe870dd62
paulief <paulfagan1991@gmail.com>
2016-06-06 17:24:52 -0700

    Add test for updating input value on change

--- __tests__/SearchBox-test.js
+++ __tests__/SearchBox-test.js
@@ -6,7 +6,21 @@ import TestUtils from 'react-addons-test-utils';
 import SearchBox from '../components/SearchBox/SearchBox';
 
 describe('<SearchBox/>', () => {
-	it('adds some maths', () => {
-		expect(1+2).toEqual(3);
-	});
+	it('updates the state when the value is changed', () => {
+		const searchBox = TestUtils.renderIntoDocument(
+			<SearchBox/>
+		);
+
+		const inputEl = TestUtils.findRenderedDOMComponentWithTag(searchBox, 'input');
+
+		// state should start with empty string
+		expect(searchBox.state).toEqual({ searchInput: '' });
+		expect(inputEl.value).toEqual('');
+
+		TestUtils.Simulate.change(inputEl, { target: { value: 'new' } });
+
+		// state should now be updated
+		expect(searchBox.state).toEqual({ searchInput: 'new' });
+		expect(inputEl.value).toEqual('new');
+	})
 });
