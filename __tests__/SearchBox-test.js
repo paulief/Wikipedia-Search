dc25b7015272a3c93987f6eb2f8217a902de26e1
paulief <paulfagan1991@gmail.com>
2016-06-06 17:04:06 -0700

    Sort out caching issue with Jest

--- __tests__/SearchBox-test.js
+++ __tests__/SearchBox-test.js
@@ -1,7 +1,12 @@
 jest.unmock('../components/SearchBox/SearchBox');
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import SearchBox from '../components/SearchBox/SearchBox';
+
 describe('<SearchBox/>', () => {
-	it('adds maths', () => {
+	it('adds some maths', () => {
 		expect(1+2).toEqual(3);
 	});
 });
