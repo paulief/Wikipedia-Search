9415eab960bd3a9966a3fc149f0b777e92290a28
paulief <paulfagan1991@gmail.com>
2016-06-07 18:09:53 -0700

    Add tests for calling the search function passed from parent

--- __tests__/SearchBox-test.js
+++ __tests__/SearchBox-test.js
@@ -5,11 +5,16 @@ import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import SearchBox from '../components/SearchBox/SearchBox';
 
+// could be augmented to take properties
+const buildSearchBox = (props = {}) => {
+	return TestUtils.renderIntoDocument(<SearchBox {...props}/>);
+}
+
 describe('<SearchBox/>', () => {
+	let searchBox;
+
 	it('updates the state when the value is changed', () => {
-		const searchBox = TestUtils.renderIntoDocument(
-			<SearchBox/>
-		);
+		searchBox = buildSearchBox();
 
 		const inputElement = TestUtils.findRenderedDOMComponentWithTag(searchBox, 'input');
 
@@ -22,5 +27,19 @@ describe('<SearchBox/>', () => {
 		// state should now be updated
 		expect(searchBox.state).toEqual({ searchInput: 'new' });
 		expect(inputElement.value).toEqual('new');
-	})
+	});
+
+	it('calls the search function passed down from parent when button is clicked', () => {
+		let submitFn = jest.genMockFunction();
+		searchBox = buildSearchBox({ submitFn: submitFn });
+
+		// set value of input
+		searchBox.setState({ searchInput: 'called search' });
+
+		const submitBtn = TestUtils.findRenderedDOMComponentWithTag(searchBox, 'button');
+		TestUtils.Simulate.click(submitBtn);
+
+		expect(submitFn).toBeCalledWith('called search');
+	});
+
 });
